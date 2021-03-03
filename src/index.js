d930f56029cea4e3204a40442d4fd7aa8762b7da
Valeriia <valeriastanis@gmail.com>
2021-03-03 20:30:31 +0100

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length == 0) {
+    return []
+  } 
+  return matrix.flatMap((element, index) => 
+    index%2==1 ? element.reverse() : element
+  );  
 }
+
+
+
